ee3c418189f4db6ba7176225b02a81c4df5a4292
Mitchel Foster <mitch@Mitchels-MacBook-Pro.local>
2017-06-19 14:49:39 -0600

    Routing has been set up so that the webpage is fully functional. Ready for testing.

--- src/components/Store/Product/Product.js
+++ src/components/Store/Product/Product.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 import './Product.css';
 
 export default function Product( { id, title, img, price, addToCart } ) {
   return (
     <div id="StoreProduct__container">
-      <img src={ img } alt="Product" width="200px" height="139.39px" />
+      <Link to={`/details/${id}`}>
+        <img src={ img } alt="Product" width="200px" height="139.39px" />
+      </Link>
       <div id="StoreProduct__details">
         <div id="StoreProduct__title">
-          <span> { title } </span>
+          <Link to={`details/${id}`} className='StoreProduct_navLink'>
+            <span> { title } </span>
+          </Link>
         </div>
         <div id="StoreProduct__addToCart" onClick={ () => { addToCart( id ) } }>
           <span> ${ price } </span>
